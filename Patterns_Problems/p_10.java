3f42be6285d2a3396936c196bf609c1811c71112
CodeHack-dot <code.hack070@gmail.com>
2026-08-06 21:49:00 +0530

    p_10 added

--- Patterns_Problems/p_10.java
+++ Patterns_Problems/p_10.java
@@ -15,7 +15,7 @@ public class p_10 {
             }
             System.out.println();
         }
-        for(int row2 = n+1; row2<=(n*2)-1;row2++) {
+        for (int row2 = n + 1; row2 <= (n * 2) - 1; row2++) {
             for (int col = 1; col <= row2 - 4; col++) {
                 System.out.print("  ");
             }
@@ -28,4 +28,39 @@ public class p_10 {
             System.out.println();
         }
     }
+
+
+////Other approach
+
+//    static void main() {
+//        int n = 5;
+//        for (int row = 1; row <= n; row++) {
+//            for (int spc = 1; spc <= n - row; spc++) {
+//                System.out.print("  ");
+//            }
+//            for (int str = 1; str <= (row * 2) - 1; str++) {
+//                System.out.print("* ");
+//            }
+//            for (int spc = 1; spc <= n - row; spc++) {
+//                System.out.print("  ");
+//            }
+//            System.out.println();
+//        }
+//        for (int row = 1; row <= n; row++) {
+//            if (row == 1) {
+//                continue;
+//            }
+//            for (int col = 1; col <= row - 1; col++) {
+//                System.out.print("  ");
+//            }
+//            for (int col = 1; col <= (n * 2) - (row * 2) + 1; col++) {
+//                System.out.print("* ");
+//            }
+//            for (int col = 1; col <= row - 1; col++) {
+//                System.out.print("  ");
+//            }
+//            System.out.println();
+//        }
+//    }
+
 }
